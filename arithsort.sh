0166deff8ebb9eedac0aa6ab1dd10709effe3b15
amitkrmaharana <ak657harley@gmail.com>
2021-01-26 08:53:26 +0530

    Took 3 inputs

--- arithsort.sh
+++ arithsort.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to the Arithmetic Computation and Sorting Program"
+echo "Input the value of 3 inputs"
+read a
+read b
+read c
+echo "1st input $a"
+echo "2nd input $b"
+echo "#rd input $c"
+
