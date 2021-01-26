4c871f9fce84429a8adc60dbb68746b3e7fdf2ab
amitkrmaharana <ak657harley@gmail.com>
2021-01-26 09:23:33 +0530

    1st Arithmetic operation successful

--- arithsort.sh
+++ arithsort.sh
@@ -4,7 +4,5 @@ echo "Input the value of 3 inputs"
 read a
 read b
 read c
-echo "1st input $a"
-echo "2nd input $b"
-echo "#rd input $c"
-
+arith1=`awk 'BEGIN{print '$a'+'$b'*'$c'}'`
+echo "1st arithmetic operation yieds value $arith1"
