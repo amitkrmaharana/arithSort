c888c32dd4aeee826bcddb34bc0bb01f03a8d0e7
amitkrmaharana <ak657harley@gmail.com>
2021-01-26 09:37:49 +0530

    3rd arithmetic operation successful

--- arithsort.sh
+++ arithsort.sh
@@ -6,5 +6,7 @@ read b
 read c
 arith1=`awk 'BEGIN{print '$a'+'$b'*'$c'}'`
 arith2=`awk 'BEGIN{print '$a'*'$b'+'$c'}'`
+arith3=`awk 'BEGIN{print '$c'+'$a'/'$b'}'`
 echo "1st arithmetic operation yieds value $arith1"
 echo "2nd arithmetic operation yieds value $arith2"
+echo "3rd arithmetic operation yieds value $arith3"
